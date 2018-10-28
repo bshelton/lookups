a248a3ac8140852c52ec8cad3c32b96a3b4192a8
bshelton <brockshelt@gmail.com>
2018-10-28 19:45:31 -0400

    updated

--- lookup.py
+++ lookup.py
@@ -2,82 +2,110 @@ import socket
 import re
 import threading
 import sys
-import timeit
+import time
 import queue
 import csv
+from datetime import datetime, timedelta
+import zipfile
+import os
 
-start = timeit.default_timer()
+numberOfThreads = int(sys.argv[1])
+minutesToSleep = 0.1
+minutesToZipFile = 3
+outputFileName = 'output.csv'
+inputFileName = 'list.txt'
 
-lines = []
-num = int(sys.argv[1])
+def formattedDateWithSeconds(date):
+    return date.strftime('%Y%m%d%H%M%S')
 
-with open('output.csv','w') as file:
-    file.write("orig_domain,recur_ip,recur_domain,timestamp\n")
+def formattedDate(date):
+    return date.strftime('%Y%m%d%H%M')
 
-    def is_ip(ip):
+def zipFile(fileName):
+    print("Zipping file " + fileName + ".zip")
+    zipfile.ZipFile(formattedDate(datetime.now()) + "_resolutions_archive" + ".zip", mode='w').write(fileName)
 
-        validIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
-        
-        aa = re.match(validIpAddressRegex,ip)
-        if aa:
-            return True
-        else:
-            return False
+def addHeaderIfNeeded():
+    headerText = "orig_domain,recur_ip,recur_domain,timestamp\n"
+    containsHeader = False
+    with open(outputFileName, 'r') as listOutputFile:
+        for line in listOutputFile:
+            if (line == headerText):
+                containsHeader = True
+            break;
+        listOutputFile.close()
+    if not containsHeader:
+        with open(outputFileName, 'w+') as outputFile:
+            outputFile.write(headerText)
 
-    def query(line):
-        flag = is_ip(line.strip())
-        current_dns = ""
-        current_ips = ""
-        recursive = []
+def isIP(ip):
+    validIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"    
+    return re.match(validIpAddressRegex, ip)
 
-        if flag == True:
-            print ("IP, needs to be converted to DNS")
+def query(line, outputFile):
+    strippedLine = line.strip()
+
+    if  isIP(strippedLine):
+        print ("IP needs to be converted to DNS")
+        try:
+            currentDNS = socket.gethostbyaddr(strippedLine)[0]
+            outputFile.write(currentDNS + "," + strippedLine + ",NO_DOMAIN," + formattedDateWithSeconds(datetime.now()) + "\n")
+        except:
+            print ("Cant resolve")
+    else:
+        # print ("DNS, need to resolve all ips")
+        currentIPs = list( map( lambda x: x[4][0], socket.getaddrinfo( \
+        line.strip(),22,type=socket.SOCK_STREAM)))
+
+        for currentIP in currentIPs:
             try:
-                current_dns = socket.gethostbyaddr(line.strip())
-                current_dns = current_dns[0]
-                
-                file.write("," + current_dns + "\n")
+                originDomain = strippedLine
+                recurDomain = socket.gethostbyaddr(currentIP)[0]
+                print (recurDomain)
+                outputFile.write(originDomain + "," + currentIP + "," + recurDomain + "," + formattedDateWithSeconds(datetime.now()) + "\n")
             except:
-                print ("Cant resolve")
-        elif flag == False:
-            print ("DNS, need to resolve all ips")
-            current_ips = list( map( lambda x: x[4][0], socket.getaddrinfo( \
-            line.strip(),22,type=socket.SOCK_STREAM)))
-            print (current_ips)
+                 print("Can't resolve domain to ip: " + currentIP)
+                 outputFile.write(originDomain + "," + currentIP + ",NO_DOMAIN," + formattedDateWithSeconds(datetime.now()) + "\n")
 
-            for i in current_ips:
-                try:
-                    temp = socket.gethostbyaddr(i)
-                    print (temp[0])
-                    file.write("," + temp[0]+ "\n")
-                except:
-                    print("Can't resolve")
-            file.write("," + str(current_ips)+ "\n")
-
-
-    with open('list.txt', 'r') as f:
-        for line in f:
-            lines.append(line)
-
-
-    stop = timeit.default_timer()
-
-    def wrapper_targetFunc(f, q):
-        while True:
+def runFile():
+    def wrapperTargetFunc(workerFunction, q, outputFile):
+        while not q.empty():
             try:
                 work = q.get(timeout=3)  # or whatever
             except queue.Empty:
-                return
-            f(work)
+                print("Error getting url from queue")
+            workerFunction(work, outputFile)
             q.task_done()
 
-    q = queue.Queue()
-    for i in lines:
-        q.put_nowait(i)
+    lines = [] #reset the input array
 
-    for _ in range(num):
-        threading.Thread(target=wrapper_targetFunc,
-                        args=(query, q)).start()
-    q.join()
+    with open(outputFileName, 'a+') as outputFile:
+        #Read lines from input file
+        with open(inputFileName, 'r') as listFile:
+            for line in listFile:
+                lines.append(line)
+            listFile.close()
 
-    print('Time: ', stop - start)
+        threadQueue = queue.Queue()
+        for line in lines:
+            threadQueue.put_nowait(line)
+
+        for _ in range(numberOfThreads):
+            threading.Thread(target=wrapperTargetFunc,
+                            args=(query, threadQueue, outputFile)).start()
+        threadQueue.join()
+        stop = datetime.now()
+        outputFile.close()
+        print('Time: ', stop - timeSinceLastZip)
+
+timeSinceLastZip = datetime.now()
+addHeaderIfNeeded()
+while True:
+    runFile()
+    if (datetime.now() - timeSinceLastZip > timedelta(minutes=minutesToZipFile)):
+        zipFile(outputFileName)
+        os.remove(outputFileName)
+        timeSinceLastZip = datetime.now()
+        
+    print("Sleeping ", minutesToSleep, " minutes...")
+    time.sleep(minutesToSleep * 60)
